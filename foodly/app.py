59ec843c1b3959e9fe285233bf2397718ac2d9a5
am12an34 <151461149+am12an34@users.noreply.github.com>
2024-01-23 01:28:42 +0530

    Update app.py

--- foodly/app.py
+++ foodly/app.py
@@ -2,9 +2,16 @@ from flask import*
 app = Flask(__name__)
 
 #-------------------------
+@app.route("/")
+def index():
+    return redirect(url_for('login'))
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+@app.route("/register")
+def login():
+    return render_template('register.html')
 
 #--------------------------
 if __name__ == '__main__':
