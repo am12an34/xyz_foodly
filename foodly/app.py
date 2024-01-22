9d3d9f139e1279a1da6d9a73dcfa5b6d9b4716bb
am12an34 <151461149+am12an34@users.noreply.github.com>
2024-01-23 01:41:57 +0530

    Update app.py

--- foodly/app.py
+++ foodly/app.py
@@ -7,6 +7,12 @@ def index():
     return redirect(url_for('login'))
 @app.route("/login")
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        #database er kaaj korbo...
+        
     return render_template('login.html')
 
 @app.route("/register")
