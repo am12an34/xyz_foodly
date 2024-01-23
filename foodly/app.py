c9d873ca1d0457ac2c8083c46b37ec1204c18a01
am12an34 <151461149+am12an34@users.noreply.github.com>
2024-01-24 02:03:08 +0530

    Update app.py

--- foodly/app.py
+++ foodly/app.py
@@ -1,11 +1,29 @@
 from flask import*
 app = Flask(__name__)
 
-#-------------------------
+#----------------------------------------------------
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' in session:
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def registration_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' in session:
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route("/")
 def index():
     return redirect(url_for('login'))
+    
 @app.route("/login")
+@login_required
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -16,10 +34,10 @@ def login():
     return render_template('login.html')
 
 @app.route("/register")
+@registration_required
 def register():
-    #condition for block_none
     return render_template('signup.html',block_none="none")
 
-#--------------------------
+#----------------------------------------------------
 if __name__ == '__main__':
     app.run(debug=True)
